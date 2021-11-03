55ca7aaee7289ae1f9f244b9a3a8df2d2dbb2084
Owen Stowe <ostowe@gmail.com>
2021-11-03 13:22:52 -0400

    Update figma format to support typography objects

--- formats/figmaTokens.js
+++ formats/figmaTokens.js
@@ -3,37 +3,66 @@
  * @param {object} tokenObj nested tokens object.
  */
 const constructRecursive = (tokenObj) => {
-  if (tokenObj.value) {
-    if (tokenObj.attributes.figmaType) {
+  const {
+    value,
+    comment,
+    attributes: {
+      description,
+      figmaType,
+    } = {},
+  } = tokenObj;
+
+  if (value) {
+    if (figmaType) {
       return {
-        value: tokenObj.value,
-        description: tokenObj.comment || '',
-        type: tokenObj.attributes.figmaType || 'other',
+        value: value,
+        description: comment || '',
+        type: figmaType || 'other',
       };
     } else {
       return null;
     }
   } else {
-    const returnObj = Object.keys(tokenObj).reduce((acc, name) => {
-      if (tokenObj[name]) {
-        const nextVal = constructRecursive(tokenObj[name]);
-
-        if (nextVal) {
+    if (figmaType && figmaType === 'typography') {
+      // Handle typography sets for figma that need to have a value set to an object.
+      const valueObject = Object.keys(tokenObj).reduce((acc, name) => {
+        if ('attributes' !== name) {
           return {
             ...acc,
-            [name]: nextVal,
+            [name]: tokenObj[name].value,
           };
         }
+      }, {});
 
-        return acc;
+      if (Object.keys(valueObject).length) {
+        return {
+          value: valueObject,
+          description: comment || description || '',
+          type: figmaType || 'other',
+        };
       }
-    }, {});
+    } else {
+      const returnObj = Object.keys(tokenObj).reduce((acc, name) => {
+        if (tokenObj[name]) {
+          const nextVal = constructRecursive(tokenObj[name]);
 
-    if (Object.keys(returnObj).length) {
-      return returnObj;
+          if (nextVal) {
+            return {
+              ...acc,
+              [name]: nextVal,
+            };
+          }
+
+          return acc;
+        }
+      }, {});
+
+      if (Object.keys(returnObj).length) {
+        return returnObj;
+      }
+
+      return null;
     }
-
-    return null;
   }
 }
 
